b00696e363a09c5e88ff90ac83b622fa9d32c5a7
perfect <18919028649@163.com>
2016-04-21 14:55:57 +0800

    first commit

---
+++ app/src/main/java/com/example/yuanyuan/commonmedia/io/vov/vitamio/R.java
@@ -0,0 +1,8 @@
+package com.example.yuanyuan.commonmedia.io.vov.vitamio;
+
+public class R {
+    public static final class raw {
+        public static final int libarm = raw.libarm;
+        public static final int pub = raw.pub;
+    }
+}
